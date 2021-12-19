3b0e92491e660c62f281348354601449aa36daa2
scaler <zengkaiwang@yeah.net>
2021-12-19 10:00:43 +0800

    处理报错

--- dist/main.js
+++ dist/main.js
@@ -15,7 +15,7 @@
 
 var _greeting = require("./greeting.js");
 
-document.write((0, _greeting.greeting)('jack')); // import { greeting } from './greeting' // 无后缀的情况暂未支持 },'./greeting.js': function (require, module, exports) { "use strict";
+document.write((0, _greeting.greeting)('jack')); },'./greeting.js': function (require, module, exports) { "use strict";
 
 Object.defineProperty(exports, "__esModule", {
   value: true
--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-// import { greeting } from './greeting' // 无后缀的情况暂未支持
 import { greeting } from './greeting.js'
 
 document.write(greeting('jack'))
